f6ea195ebf2197dd188fbd9a4280f86141b22907
SilviaVilar <93494776+SilviaVilar@users.noreply.github.com>
2025-07-03 14:55:22 +0200

    Update database.php
    Problema SSL

--- config/database.php
+++ config/database.php
@@ -59,7 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/BaltimoreCyberTrustRoot.crt.pem'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', 'storage/certs/BaltimoreCyberTrustRoot.crt.pem'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
