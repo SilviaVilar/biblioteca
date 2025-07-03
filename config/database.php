04ed735d1a90acee7907fdf20b1e0dff3d47288a
SilviaVilar <93494776+SilviaVilar@users.noreply.github.com>
2025-07-03 17:13:25 +0200

    Update database.php
    SSL array_filter

--- config/database.php
+++ config/database.php
@@ -58,7 +58,8 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => [
                 //PDO::MYSQL_ATTR_SSL_CA => base_path('storage/certs/BaltimoreCyberTrustRoot.crt.pem'),
                 PDO::MYSQL_ATTR_SSL_CA => '/home/site/wwwroot/storage/certs/BaltimoreCyberTrustRoot.crt.pem',
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
