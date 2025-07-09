a50f0fc731e465bc2fbc3ea45ec16ab75698c1f8
SilviaVilar <93494776+SilviaVilar@users.noreply.github.com>
2025-07-09 18:11:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-nginx -c /home/site/wwwroot/nginx.conf
+
+# Mover nginx.conf al lugar correcto
+mv /home/site/wwwroot/nginx.conf /etc/nginx/nginx.conf
+
+# Permisos para nginx.conf
+chmod 644 /etc/nginx/nginx.conf
+
+# Recargar nginx para que coja la configuración
+nginx -s reload
+
+# Arrancar php-fpm si no está en ejecución
+if ! pgrep php-fpm > /dev/null; then
+    php-fpm -D
+fi
+
+# Arrancar nginx en primer plano (requisito contenedor)
+nginx -g 'daemon off;'
