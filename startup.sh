989efc7a643ef4128f790c6006fea9e5f1997af1
SilviaVilar <93494776+SilviaVilar@users.noreply.github.com>
2025-07-09 22:41:39 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,9 +5,10 @@ if [ -f /home/site/wwwroot/nginx.conf ]; then
     cp /home/site/wwwroot/nginx.conf /etc/nginx/nginx.conf
     chmod 644 /etc/nginx/nginx.conf
 fi
-
-# Arrancar php-fpm en background
-php-fpm -D
+# Iniciar PHP-FPM si no está en ejecución
+if ! pgrep php-fpm > /dev/null; then
+    php-fpm -y /usr/local/etc/php-fpm.conf -D
+fi
 
 # Arrancar nginx en primer plano (requisito contenedor)
 nginx -g 'daemon off;'
